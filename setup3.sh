8db15351cf04c78eda1b77e9472d272b78aaa737
Kaito <mail.kaito03@gmail.com>
2019-12-09 18:21:04 +0900

    add setup script for python3

---
+++ setup3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# RUN: source setup.sh
+
+python3 -m venv env && source env/bin/activate
+pip install -r requirements.txt
+python3 manage.py makemigrations && python3 manage.py migrate
+python3 manage.py loaddata fixture.json
